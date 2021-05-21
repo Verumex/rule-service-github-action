b27ee741ff3a634046c456eb1bf2fc638ab99d48
Paul <hiattp@gmail.com>
2021-05-21 08:05:06 -0600

    TEMP: comment out experimental spec

--- index.test.js
+++ index.test.js
@@ -1,7 +1,7 @@
 const wait = require('./wait');
-const process = require('process');
-const cp = require('child_process');
-const path = require('path');
+// const process = require('process');
+// const cp = require('child_process');
+// const path = require('path');
 
 test('sample throw expectation', async () => {
   await expect(wait('foo')).rejects.toThrow('milliseconds not a number');
@@ -16,11 +16,11 @@ test('sample async', async () => {
 });
 
 // shows how the runner will run a javascript action with env / stdout protocol
-test('test run', () => {
-  process.env['INPUT_API_HOST'] = 'SET ME';
-  process.env['INPUT_AUTH_TOKEN'] = 'SET ME';
-  process.env['INPUT_RULE_SERVICE_ID'] = 'SET ME';
-  process.env['INPUT_VALIDATION_SCOPE'] = 'SET ME';
-  const ip = path.join(__dirname, 'index.js');
-  console.log(cp.execSync(`node ${ip}`, {env: process.env}).toString());
-})
+// test('test run', () => {
+//   process.env['INPUT_API_HOST'] = 'SET ME';
+//   process.env['INPUT_AUTH_TOKEN'] = 'SET ME';
+//   process.env['INPUT_RULE_SERVICE_ID'] = 'SET ME';
+//   process.env['INPUT_VALIDATION_SCOPE'] = 'SET ME';
+//   const ip = path.join(__dirname, 'index.js');
+//   console.log(cp.execSync(`node ${ip}`, {env: process.env}).toString());
+// })
